4d915f6f28d3df1ff1bf8e55d896fd6aa9e0dd34
danilo <danilogprojetos@gmail.com>
2017-03-10 16:21:24 -0300

    Criando a Classe de conexão banco para testar CRUD e JPA

--- src/com/banco/dao/Conexaobanco.java
+++ src/com/banco/dao/Conexaobanco.java
@@ -2,11 +2,40 @@ package com.banco.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Conexaobanco {
 
-	
+	// Nome do usuário do mysql
+	private static final String USERNAME = "root";
+
+	// Senha do mysql
+	private static final String PASSWORD = "root";
+
+	// Dados de caminho, porta e nome da base de dados que irá ser feita a
+	// conexão
+	private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/teste_viverde";
+
+
+	public static Connection createConnectionToMySQL() throws Exception {
+		Class.forName("org.postgresql.Driver"); // Faz com que a classe seja
+												// carregada pela JVM
+
+		// Cria a conexão com o banco de dados
+		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+
+		return connection;
 	}
 
+	public static void main(String[] args) throws Exception {
+
+		// Recupera uma conexão com o banco de dados
+		Connection con = createConnectionToMySQL();
+
+		// Testa se a conexão é nula
+		if (con != null) {
+			System.out.println("Conexão obtida com sucesso!" + con);
+			con.close();
+		}
+
+	}
+}
